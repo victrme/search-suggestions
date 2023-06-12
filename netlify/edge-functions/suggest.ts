cdb867bdd9695c3d8c69d1f352a5aaa531e59f5b
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-12 14:26:41 +0200

    Added Qwant, separated fetch from API parsing

--- netlify/edge-functions/suggest.ts
+++ netlify/edge-functions/suggest.ts
@@ -8,10 +8,6 @@ type Suggestions = {
 	detail?: Detail
 }[]
 
-const headers = {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
-}
-
 const providerURLs = {
 	bing: 'https://www.bing.com/AS/Suggestions?mkt=%l&qry=%q&cvid=9ECCF1FD07F64EA48B12A0CE5819B9BC',
 	google: 'https://www.google.com/complete/search?q=%q&hl=%l&cp=2&client=gws-wiz&xssi=t',
@@ -21,17 +17,58 @@ const providerURLs = {
 	startpage: 'https://www.startpage.com/suggestions?q=%q&lui=english&sc=i9RGhXphNiwC20',
 }
 
+function requestProviderAPI(url: string) {
+	const r = fetch(url, {
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
+		},
+	})
+
+	return {
+		json: async (): Promise<unknown | undefined> => {
+			try {
+				return await (await r).json()
+			} catch (_) {
+				console.warn("Can't parse JSON")
+			}
+		},
+		text: async (): Promise<string | undefined> => {
+			try {
+				return await (await r).text()
+			} catch (_) {
+				console.warn("Can't parse to text")
+			}
+		},
+	}
+}
+
+async function qwant(q: string): Promise<Suggestions> {
+	type QwantAPI = {
+		status: string
+		data: {
+			items: { value: string }[]
+			special: []
+		}
+	}
+
+	const url = providerURLs.qwant.replace('%q', q).replace('&locale=%l', '')
+	const json = await requestProviderAPI(url).json()
+
+	if (json && (json as QwantAPI).status === 'success') {
+		return (json as QwantAPI).data.items.map((item) => ({ text: item.value }))
+	}
+
+	return []
+}
+
 async function duckduckgo(q: string): Promise<Suggestions> {
 	type DuckduckgoAPI = { phrase: string }[]
 
-	try {
-		const url = providerURLs.duckduckgo.replace('%q', q)
-		const response = await fetch(url, { headers })
-		const json = await response.json()
+	const url = providerURLs.duckduckgo.replace('%q', q)
+	const json = await requestProviderAPI(url).json()
+
+	if (json) {
 		return Object.values(json as DuckduckgoAPI).map((item) => ({ text: item.phrase }))
-	} catch (error) {
-		console.warn("Can't get suggestions")
-		console.warn(error)
 	}
 
 	return []
@@ -39,9 +76,24 @@ async function duckduckgo(q: string): Promise<Suggestions> {
 
 export default async (request: Request) => {
 	const { pathname } = new URL(request.url)
-	const ddg = await duckduckgo(pathname.replace('/', ''))
+	const provider = pathname.slice(1, pathname.slice(1).indexOf('/') + 1)
+	const query = pathname.slice(provider.length + 2)
 
-	console.log(ddg)
+	let result: Suggestions = []
 
-	return Response.json({ hello: 'world' })
+	switch (provider) {
+		case 'duckduckgo':
+			result = await duckduckgo(query)
+			break
+
+		case 'qwant':
+			result = await qwant(query)
+			break
+
+		default:
+			result = []
+			break
+	}
+
+	return Response.json(result)
 }
