6168bc54b9d3ac3352a8a52b0d6f4f1d48c661cb
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-29 10:35:47 +0200

    Added Nullish coalescing operator (big words)

--- cloudflare-worker/worker.ts
+++ cloudflare-worker/worker.ts
@@ -4,9 +4,9 @@ export default {
 	async fetch(request: Request): Promise<Response> {
 		const { pathname } = new URL(request.url)
 		const cat = pathname.split('/')
-		const provider = cat[1]
-		const lang = cat[2]
-		const query = pathname.slice(lang.length + provider.length + 3)
+		const provider = cat[1] ?? ''
+		const lang = cat[2] ?? ''
+		const query = pathname.slice(lang.length + provider.length + 3) ?? ''
 
 		let result = await handler({ lang, provider, query })
 
