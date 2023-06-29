da718379cfd16c705e7b4c919c7e8cb67787a99d
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-29 10:32:20 +0200

    Old parameters for cloudflare worker

--- cloudflare-worker/worker.ts
+++ cloudflare-worker/worker.ts
@@ -3,12 +3,12 @@ import handler from '../_handler'
 export default {
 	async fetch(request: Request): Promise<Response> {
 		const { pathname } = new URL(request.url)
+		const cat = pathname.split('/')
+		const provider = cat[1]
+		const lang = cat[2]
+		const query = pathname.slice(lang.length + provider.length + 3)
 
-		let result = await handler({
-			lang: 'en',
-			provider: 'duckduckgo',
-			query: pathname.replace('/', '') ?? '',
-		})
+		let result = await handler({ lang, provider, query })
 
 		return new Response(JSON.stringify(result), {
 			headers: {
