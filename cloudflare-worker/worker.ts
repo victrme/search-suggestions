ffdefde766e53e51f17a71428af837f09726e7a7
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-10-13 21:48:10 +0200

    Close socket on subrequest limit

--- cloudflare-worker/worker.ts
+++ cloudflare-worker/worker.ts
@@ -10,13 +10,23 @@ export default {
 
 		const webSocketPair = new WebSocketPair()
 		const [client, server] = Object.values(webSocketPair)
+		let count = 0
 
 		server.accept()
 
 		server.addEventListener('message', (event) => {
-			handler(event.data.toString()).then((response) => {
+			const url = event?.data?.toString() ?? ''
+
+			if (count === 50) {
+				server.close()
+				return
+			}
+
+			handler(url).then((response) => {
 				server.send(JSON.stringify(response))
 			})
+
+			count++
 		})
 
 		return new Response(null, {
