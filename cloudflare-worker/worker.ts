3dd6e1a3f93df7914f055d0d0d66d752933c6bcc
Victor Azevedo <victor.azevedo@mail.com>
2023-10-27 10:25:28 +0200

    Added GET compatibility in cf-worker

--- cloudflare-worker/worker.ts
+++ cloudflare-worker/worker.ts
@@ -1,62 +1,84 @@
 import handler from '../src'
 
-let server: WebSocket
-let client: WebSocket
-
 export default {
 	async fetch(request: Request) {
-		const upgradeHeader = request.headers.get('Upgrade')
-
-		if (!upgradeHeader || upgradeHeader !== 'websocket') {
-			return new Response('Expected Upgrade: websocket', { status: 426 })
+		// When using GET method
+		if (request.method === 'GET') {
+			return await responseAsHttp(request)
 		}
 
-		let subRequestCount = 0
-		const webSocketPair = new WebSocketPair()
-		client = webSocketPair[0]
-		server = webSocketPair[1]
+		// When using WS method
+		if (request.method === 'WS') {
+			if (request.headers.get('Upgrade') !== 'websocket') {
+				return new Response('Expected Upgrade: websocket', { status: 426 })
+			}
 
-		server.accept()
+			return createWebsocket()
+		}
 
-		server.addEventListener(
-			'message',
-			debounce((ev: MessageEvent) => {
-				if (subRequestCount++ === 50) {
-					subRequestCount = 0
-					server.close()
-					return
-				}
-
-				sendMessage(ev)
-			}, 150)
-		)
-
-		return new Response(null, {
-			status: 101,
-			webSocket: client,
-		})
+		return new Response('', { status: 405 })
 	},
 }
 
-function sendMessage(event: MessageEvent) {
-	try {
-		const data = JSON.parse(event.data.toString() ?? '{}')
+async function responseAsHttp(request: Request): Promise<Response> {
+	const url = new URL(request.url)
+	const params = new URLSearchParams(url.searchParams)
 
-		const response = handler({
-			q: data.q ?? '',
-			with: data.with ?? '',
-			lang: data.lang ?? '',
-		})
+	const result = await handler({
+		q: params.get('q') ?? '',
+		lang: params.get('l') ?? 'en',
+		with: params.get('with') ?? 'duckduckgo',
+	})
 
-		response.then((response) => {
-			server.send(JSON.stringify(response))
-		})
+	return new Response(JSON.stringify(result), {
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+		},
+	})
+}
 
-		//
-	} catch (error) {
-		console.error(error)
-		server.send('{error: ' + error + '}')
-	}
+function createWebsocket() {
+	let subRequestCount = 0
+	const webSocketPair = new WebSocketPair()
+	const [client, server] = Object.values(webSocketPair)
+
+	server.accept()
+
+	server.addEventListener(
+		'message',
+		debounce((ev: MessageEvent) => {
+			if (subRequestCount++ === 50) {
+				subRequestCount = 0
+				server.close()
+				return
+			}
+
+			try {
+				const data = JSON.parse(ev.data.toString() ?? '{}')
+
+				const response = handler({
+					q: data.q ?? '',
+					with: data.with ?? '',
+					lang: data.lang ?? '',
+				})
+
+				response.then((response) => {
+					server.send(JSON.stringify(response))
+				})
+
+				//
+			} catch (error) {
+				console.error(error)
+				server.send('{error: ' + error + '}')
+			}
+		}, 150)
+	)
+
+	return new Response(null, {
+		status: 101,
+		webSocket: client,
+	})
 }
 
 function debounce(callback: Function, delay: number) {
